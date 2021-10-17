465a58d7a483ad4b4bd67e39c0ee5079dedab9c7
megan <me.maldy@gmail.com>
2021-10-17 16:10:05 -0700

    commit on front-end-testing

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
 function App() {
   return (
     <IonApp>
