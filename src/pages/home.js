e8cb36f70fddb447be847b86df7112119b01cfdf
Megan Maldonado <me.maldy@gmail.com>
2021-10-30 18:33:18 -0700

    working on questions

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,16 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonSelect, IonSelectOption, IonButton, } from '@ionic/react';
-import { useForm } from 'react-hook-form';
-const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+  IonButton
+} from "@ionic/react";
+
 const Home = () => {
   return (
     <IonPage>
@@ -11,33 +20,36 @@ const Home = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-<IonToolbar>
-<form>
-<IonItem>
-  
-          <IonLabel>Region of Intrest</IonLabel>
-          <IonSelect>
-            <IonSelectOption value="Europe">Europe</IonSelectOption>
-            <IonSelectOption value="Asia">Asia</IonSelectOption>
-            <IonSelectOption value="Latin America">Latin America</IonSelectOption>
-            <IonSelectOption value="North America">North Americ</IonSelectOption>
-            <IonSelectOption value="Middle East">Middle East</IonSelectOption>
-            <IonSelectOption value="Africa">Africa</IonSelectOption>
-            
-          </IonSelect>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Budget</IonLabel>
-          <IonSelect>
-          <IonSelectOption value="$">$</IonSelectOption>
-          <IonSelectOption value="$$">$$</IonSelectOption>
-          <IonSelectOption value="$$$">$$$</IonSelectOption>
-          </IonSelect>
-         
-        </IonItem>
-<IonButton value="submit">Submit</IonButton>
-        </form>
-</IonToolbar>
+        <IonToolbar>
+          <form>
+            <IonItem>
+              <IonLabel>Region of Intrest</IonLabel>
+              <IonSelect>
+                <IonSelectOption value="Europe">Europe</IonSelectOption>
+                <IonSelectOption value="Asia">Asia</IonSelectOption>
+                <IonSelectOption value="Latin America">
+                  Latin America
+                </IonSelectOption>
+                <IonSelectOption value="North America">
+                  North Americ
+                </IonSelectOption>
+                <IonSelectOption value="Middle East">
+                  Middle East
+                </IonSelectOption>
+                <IonSelectOption value="Africa">Africa</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Budget</IonLabel>
+              <IonSelect>
+                <IonSelectOption value="$">$</IonSelectOption>
+                <IonSelectOption value="$$">$$</IonSelectOption>
+                <IonSelectOption value="$$$">$$$</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonButton value="submit">Submit</IonButton>
+          </form>
+        </IonToolbar>
       </IonContent>
     </IonPage>
   );
