9e1dca5eb3aaa3924e080e72e7553a616039716a
megan <me.maldy@gmail.com>
2021-11-08 21:12:12 -0800

    on signupClick Fix

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -11,9 +11,9 @@ const SignUp = () => {
   const onPasswordChange = useCallback((e) => setPassword(e.detail?.value), []);
   const onConfirmPasswordChange = useCallback((e) => setConfirmPassword(e.detail?.value), []);
 
-  const onSignupClick = useCallback (() =>{
-      console.log(confirmpassword, password);
-  },[confirmpassword, password]);
+  // const onSignupClick = useCallback (() =>{
+  //     console.log(confirmpassword, password);
+  // },[confirmpassword, password]);
 
   
   return (
