0f54c8636affb0f45eef5e271beafb95dc28f5b1
megan <me.maldy@gmail.com>
2021-11-10 16:46:31 -0800

    test

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,8 @@
-import { useState} from 'react';
-// import useFetchData from "../hooks/fetch_data";
+/** @format */
 
+import { useState } from "react";
+// import useFetchData from "../hooks/fetch_data";
+//import { useForm } from "react-hook-form";
 import {
   IonContent,
   IonHeader,
@@ -18,31 +20,29 @@ import {
 const Home = () => {
   //const { data, loading } = useFetchData();
   const [showModal, setShowModal] = useState(false);
-  
-  
-    // axios
-    //   .get("http://127.0.0.1:8000/dataset")
-    //   .then((response) => {
-    //     console.log(response);
-    //     return response.data();
-    //   })
-    //   .then((data) => {
-    //     const recommendPlaces = data.results.map((placeReccomend) => {
-    //       return {
-    //         region: placeReccomend.region,
-    //         budget: placeReccomend.cost,
-    //         wifi: placeReccomend.wifi,
-    //         activity: placeReccomend.activity,
-    //         healthcare: placeReccomend.healthcare,
-    //         identity: placeReccomend.lgbt,
-    //         coffee: placeReccomend.coffee,
-    //         safety: placeReccomend.safety,
-    //         transportation: placeReccomend.walk_drive,
-    //       };
-    //     });
-    //     setReccomend(recommendPlaces);
-    //   });
- 
+  const [region, setRegion] = useState();
+  // axios
+  //   .get("http://127.0.0.1:8000/dataset")
+  //   .then((response) => {
+  //     console.log(response);
+  //     return response.data();
+  //   })
+  //   .then((data) => {
+  //     const recommendPlaces = data.results.map((placeReccomend) => {
+  //       return {
+  //         region: placeReccomend.region,
+  //         budget: placeReccomend.cost,
+  //         wifi: placeReccomend.wifi,
+  //         activity: placeReccomend.activity,
+  //         healthcare: placeReccomend.healthcare,
+  //         identity: placeReccomend.lgbt,
+  //         coffee: placeReccomend.coffee,
+  //         safety: placeReccomend.safety,
+  //         transportation: placeReccomend.walk_drive,
+  //       };
+  //     });
+  //     setReccomend(recommendPlaces);
+  //   });
 
   return (
     <IonPage>
@@ -58,96 +58,112 @@ const Home = () => {
               <IonTitle>home</IonTitle>
             </IonToolbar>
           </IonHeader>
+          
+            <IonItem>
+              <IonLabel>Region of Intrest</IonLabel>
+              <IonSelect value={region} multiple={true} onIonChange={e => setRegion(e.detail.value)}>
+                <IonSelectOption value="europe">Europe</IonSelectOption>
+                <IonSelectOption value="asia">Asia</IonSelectOption>
+                <IonSelectOption value="latinAmerica">
+                  Latin America
+                </IonSelectOption>
+                <IonSelectOption value="northAmerica">
+                  North America
+                </IonSelectOption>
+                <IonSelectOption value="middleEast">
+                  Middle East
+                </IonSelectOption>
+                <IonSelectOption value="africa">Africa</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Budget</IonLabel>
+              <IonSelect>
+                <IonSelectOption value="$">$</IonSelectOption>
+                <IonSelectOption value="$$">$$</IonSelectOption>
+                <IonSelectOption value="$$$">$$$</IonSelectOption>
+                <IonSelectOption value="$$$">$$$$</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Wifi Connection</IonLabel>
+              <IonSelect>
+                <IonSelectOption value="Yes">Yes</IonSelectOption>
+                <IonSelectOption value="No">No</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Types of activities</IonLabel>
+              <IonSelect>
+                <IonSelectOption value="nightlife">nightlife</IonSelectOption>
+                <IonSelectOption value="fun">Fun</IonSelectOption>
+                <IonSelectOption value="work">Work</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Health Care Importance</IonLabel>
+              <IonSelect>
+                <IonSelectOption value="1">+</IonSelectOption>
+                <IonSelectOption value="2">++</IonSelectOption>
+                <IonSelectOption value="3">+++</IonSelectOption>
+                <IonSelectOption value="4">++++</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Yes I Love Coffe, but How Much</IonLabel>
+              <IonSelect>
+                <IonSelectOption value="0">nope</IonSelectOption>
+                <IonSelectOption value="1">
+                  I can take it or leave it
+                </IonSelectOption>
+                <IonSelectOption value="2">I kind of like it</IonSelectOption>
+                <IonSelectOption value="3">yea, sure</IonSelectOption>
+                <IonSelectOption value="4">umm yea!</IonSelectOption>
+                <IonSelectOption value="5">I F-ing love it!</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Is safety important to you</IonLabel>
+              <IonSelect multiple={true}>
+                <IonSelectOption value="yes">Yes</IonSelectOption>
+                <IonSelectOption value="no">No</IonSelectOption>
+                <IonSelectOption value="lgbt">LGBT Friendly</IonSelectOption>
+                <IonSelectOption value="female">
+                  Female Friendly
+                </IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Preferred mode of trasportation</IonLabel>
+              <IonSelect multiple={true}>
+                <IonSelectOption value="walk" >Walk</IonSelectOption>
+                <IonSelectOption value="drive">Drive</IonSelectOption>
+                <IonSelectOption value="none">
+                  None of the Above
+                </IonSelectOption>
+              </IonSelect>
+            </IonItem>
 
-          <IonItem>
-            <IonLabel>Region of Intrest</IonLabel>
-            <IonSelect multiple={true}>
-              <IonSelectOption value="europe">Europe</IonSelectOption>
-              <IonSelectOption value="asia">Asia</IonSelectOption>
-              <IonSelectOption value="latinAmerica">
-                Latin America
-              </IonSelectOption>
-              <IonSelectOption value="northAmerica">
-                North America
-              </IonSelectOption>
-              <IonSelectOption value="middleEast">Middle East</IonSelectOption>
-              <IonSelectOption value="africa">Africa</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Budget</IonLabel>
-            <IonSelect>
-              <IonSelectOption value="$">$</IonSelectOption>
-              <IonSelectOption value="$$">$$</IonSelectOption>
-              <IonSelectOption value="$$$">$$$</IonSelectOption>
-              <IonSelectOption value="$$$">$$$$</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Wifi Connection</IonLabel>
-            <IonSelect>
-              <IonSelectOption value="Yes">Yes</IonSelectOption>
-              <IonSelectOption value="No">No</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Types of activities</IonLabel>
-            <IonSelect>
-              <IonSelectOption value="nightlife">nightlife</IonSelectOption>
-              <IonSelectOption value="fun">Fun</IonSelectOption>
-              <IonSelectOption value="work">Work</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Health Care Importance</IonLabel>
-            <IonSelect>
-              <IonSelectOption value="1">+</IonSelectOption>
-              <IonSelectOption value="2">++</IonSelectOption>
-              <IonSelectOption value="3">+++</IonSelectOption>
-              <IonSelectOption value="4">++++</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Yes I Love Coffe, but How Much</IonLabel>
-            <IonSelect>
-              <IonSelectOption value="0">nope</IonSelectOption>
-              <IonSelectOption value="1">
-                I can take it or leave it
-              </IonSelectOption>
-              <IonSelectOption value="2">I kind of like it</IonSelectOption>
-              <IonSelectOption value="3">yea, sure</IonSelectOption>
-              <IonSelectOption value="4">umm yea!</IonSelectOption>
-              <IonSelectOption value="5">I F-ing love it!</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Is safety important to you</IonLabel>
-            <IonSelect multiple={true}>
-              <IonSelectOption value="yes">Yes</IonSelectOption>
-              <IonSelectOption value="no">No</IonSelectOption>
-              <IonSelectOption value="lgbt">LGBT Friendly</IonSelectOption>
-              <IonSelectOption value="female">Female Friendly</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Preferred mode of trasportation</IonLabel>
-            <IonSelect multiple={true}>
-              <IonSelectOption value="walk">Walk</IonSelectOption>
-              <IonSelectOption value="drive">Drive</IonSelectOption>
-              <IonSelectOption value="none">None of the Above</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-
-          <IonButton value="submit" onClick={() => setShowModal(false)}>
-            Submit
-          </IonButton>
+            <IonButton
+              value="submit"
+              expand="block"
+            >
+              Submit
+            </IonButton>
+              <IonButton
+              value="submit"
+              expand="block"
+              onClick={() => setShowModal(false)}
+            >
+              Show results
+            </IonButton>
+    
         </IonModal>
         <IonButton onClick={() => setShowModal(true)}>
           Where would you like to go
         </IonButton>
-        <IonItem>
-          
-        </IonItem>
+
+        <IonItem></IonItem>
       </IonContent>
     </IonPage>
   );
