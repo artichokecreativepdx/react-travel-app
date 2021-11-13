4b1b1e1856eebcf6a87781ce3d422e875d9703fc
ucos <ucos4989@gmail.com>
2021-11-13 00:48:57 -0800

    Change get url

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,6 @@
-/** @format */
-
 import { useState, useEffect } from "react";
 
-import logoLong from "../img/planit_adventures.png";
+import logoLong from '../img/planit_adventures.png'
 import {
   IonContent,
   IonHeader,
@@ -19,27 +17,31 @@ import {
 import axios from "axios";
 
 const Home = () => {
+  
   const [showModal, setShowModal] = useState(false);
   const [country, setCountry] = useState();
   const [cost, setCost] = useState();
   const [wifi, setWifi] = useState();
   const [activities, setActivities] = useState();
   const [healthcare, setHealthcare] = useState();
+  const [coffee, setCoffee] = useState();
   const [safety, setSafety] = useState();
-  const [identity, setIdentity] = useState();
   const [transportation, setTransportation] = useState();
   const options = {
-    cssClass: "my-custom-interface",
+    cssClass: 'my-custom-interface'
   };
+  
+  
 
   useEffect(() => {
     axios.get("http://3.138.163.56:8000/dataset").then((response) => {
       console.log(response);
+      
     });
   });
   const callApi = () => {
     console.log(country);
-    axios.get(`/recommend`, {
+    axios.get('http://3.138.163.56:8000/recommend', {
       params: {
         region: country,
         cost: cost,
@@ -47,23 +49,27 @@ const Home = () => {
         activity: activities,
         healthcare: healthcare,
         lgbt: safety,
-        
-        walk_drive: transportation,
+        coffee: coffee,
+        walk_drive: transportation ,
       },
     });
   };
+ 
+  
 
   return (
     <IonPage>
       <IonHeader className="logo_splash">
-        <img src={logoLong} alt="planit logo" />
-      </IonHeader>
-      <IonToolbar>
-        <IonItem>
+      
+              
+              <img src={logoLong} alt="planit logo"/>
+              </IonHeader> 
+        <IonToolbar>
+          <IonItem>
           <IonTitle>home</IonTitle>
-        </IonItem>
-      </IonToolbar>
-
+          </IonItem>
+        </IonToolbar>
+     
       <IonContent>
         <IonModal isOpen={showModal}>
           <IonHeader>
@@ -71,142 +77,136 @@ const Home = () => {
               <IonTitle>home</IonTitle>
             </IonToolbar>
           </IonHeader>
+         
+            <IonItem>
+              <IonLabel>Region of Intrest</IonLabel>
 
-          <IonItem>
-            <IonLabel>Region of Intrest</IonLabel>
+              <IonSelect interfaceOptions={options} value={country} placeholder="select one" onIonChange={e => setCountry(e.detail.value)}>
+                <IonSelectOption value="Europe">Europe</IonSelectOption>
+                <IonSelectOption value="Asia">Asia</IonSelectOption>
+                <IonSelectOption value="Latin America">
 
-            <IonSelect
-              interfaceOptions={options}
-              value={country}
-              placeholder="select one"
-              onIonChange={(e) => setCountry(e.detail.value)}
-            >
-              <IonSelectOption value="Europe">Europe</IonSelectOption>
-              <IonSelectOption value="Asia">Asia</IonSelectOption>
-              <IonSelectOption value="Latin America">
-                Latin America
-              </IonSelectOption>
-              <IonSelectOption value="North America">
-                North America
-              </IonSelectOption>
-              <IonSelectOption value="Middle East">Middle East</IonSelectOption>
-              <IonSelectOption value="Africa">Africa</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
+                  Latin America
+                </IonSelectOption>
+                <IonSelectOption value="North America">
+                  North America
+                </IonSelectOption>
+                <IonSelectOption value="Middle East">
+                  Middle East
+                </IonSelectOption>
+                <IonSelectOption value="Africa">Africa</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+
             <IonLabel>Budget</IonLabel>
 
-            <IonSelect
-              interfaceOptions={options}
-              value={cost}
-              placeholder="select one"
-              onIonChange={(e) => setCost(e.detail.value)}
-            >
-              <IonSelectOption value="1">$</IonSelectOption>
-              <IonSelectOption value="2">$$</IonSelectOption>
-              <IonSelectOption value="3">$$$</IonSelectOption>
-              <IonSelectOption value="4">$$$$</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
+            <IonSelect interfaceOptions={options} value={cost} placeholder="select one" onIonChange={e => setCost(e.detail.value)}>
+
+                <IonSelectOption value="1">$</IonSelectOption>
+                <IonSelectOption value="2">$$</IonSelectOption>
+                <IonSelectOption value="3">$$$</IonSelectOption>
+                <IonSelectOption value="4">$$$$</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+
             <IonLabel>Access to Wifi</IonLabel>
 
-            <IonSelect
-              interfaceOptions={options}
-              value={wifi}
-              placeholder="select one"
-              onIonChange={(e) => setWifi(e.detail.value)}
-            >
-              <IonSelectOption value="yes">Yes</IonSelectOption>
-              <IonSelectOption value="no">No</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Is safety important to you</IonLabel>
+            <IonSelect interfaceOptions={options} value={wifi} placeholder="select one" onIonChange={e => setWifi(e.detail.value)}>
+                <IonSelectOption value="yes">Yes</IonSelectOption>
+                <IonSelectOption value="no">No</IonSelectOption>
 
-            <IonSelect
-              interfaceOptions={options}
-              value={safety}
-              placeholder="select one"
-              onIonChange={(e) => setSafety(e.detail.value)}
-            >
-              <IonSelectOption value="1">+</IonSelectOption>
-              <IonSelectOption value="2">++</IonSelectOption>
-              <IonSelectOption value="3">+++</IonSelectOption>
-              <IonSelectOption value="4">++++</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Female or LGBT Friendly</IonLabel>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Types of activities</IonLabel>
 
-            <IonSelect
-              interfaceOptions={options}
-              value={identity}
-              placeholder="select one"
-              onIonChange={(e) => setIdentity(e.detail.value)}
-            >
-              <IonSelectOption value="lgbt">LGBT Friendly</IonSelectOption>
-              <IonSelectOption value="female">Female Friendly</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Types of activities</IonLabel>
+              <IonSelect interfaceOptions={options} value={activities} placeholder="select one" onIonChange={e => setActivities(e.detail.value)}>
 
-            <IonSelect
-              interfaceOptions={options}
-              value={activities}
-              placeholder="select one"
-              onIonChange={(e) => setActivities(e.detail.value)}
-            >
-              <IonSelectOption value="nightlife">nightlife</IonSelectOption>
-              <IonSelectOption value="fun">Fun</IonSelectOption>
-              <IonSelectOption value="work">Work</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Health Care Importance</IonLabel>
+                <IonSelectOption value="nightlife">nightlife</IonSelectOption>
+                <IonSelectOption value="fun">Fun</IonSelectOption>
+                <IonSelectOption value="work">Work</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Health Care Importance</IonLabel>
 
-            <IonSelect
-              interfaceOptions={options}
-              value={healthcare}
-              placeholder="select one"
-              onIonChange={(e) => setHealthcare(e.detail.value)}
-            >
-              <IonSelectOption value="1">+</IonSelectOption>
-              <IonSelectOption value="2">++</IonSelectOption>
-              <IonSelectOption value="3">+++</IonSelectOption>
-              <IonSelectOption value="4">++++</IonSelectOption>
-            </IonSelect>
-          </IonItem>
+              <IonSelect interfaceOptions={options} value={healthcare} placeholder="select one" onIonChange={e => setHealthcare(e.detail.value)}>
 
-          <IonItem>
-            <IonLabel>Preferred mode of trasportation</IonLabel>
 
-            <IonSelect
-              interfaceOptions={options}
-              value={transportation}
-              placeholder="select one"
-              onIonChange={(e) => setTransportation(e.detail.value)}
-            >
-              <IonSelectOption value="walk">Walk</IonSelectOption>
-              <IonSelectOption value="drive">Drive</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <IonButton expand="block" value="submit" onClick={callApi}>
+                <IonSelectOption value="1">+</IonSelectOption>
+                <IonSelectOption value="2">++</IonSelectOption>
+                <IonSelectOption value="3">+++</IonSelectOption>
+                <IonSelectOption value="4">++++</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Yes I Love Coffe, but How Much</IonLabel>
+
+              <IonSelect interfaceOptions={options} value={coffee} placeholder="select one" onIonChange={e => setCoffee(e.detail.value)}>
+
+                <IonSelectOption value="0">nope</IonSelectOption>
+                <IonSelectOption value="1">
+                  I can take it or leave it
+                </IonSelectOption>
+                <IonSelectOption value="2">I kind of like it</IonSelectOption>
+                <IonSelectOption value="3">yea, sure</IonSelectOption>
+                <IonSelectOption value="4">umm yea!</IonSelectOption>
+                <IonSelectOption value="5">I F-ing love it!</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Is safety important to you</IonLabel>
+
+              <IonSelect interfaceOptions={options} value={safety} placeholder="select one" onIonChange={e => setSafety(e.detail.value)}>
+                <IonSelectOption value="1">Yes</IonSelectOption>
+                <IonSelectOption value="2">No</IonSelectOption>
+                <IonSelectOption value="3">LGBT Friendly</IonSelectOption>
+                <IonSelectOption value="4">
+
+                  Female Friendly
+                </IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Preferred mode of trasportation</IonLabel>
+
+              <IonSelect interfaceOptions={options} value={transportation} placeholder="select one" onIonChange={e => setTransportation(e.detail.value)}>
+
+                <IonSelectOption value="walk">Walk</IonSelectOption>
+                <IonSelectOption value="drive">Drive</IonSelectOption>
+                <IonSelectOption value="none">
+                  None of the Above
+                </IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonButton 
+            expand="block"
+            value="submit" onClick={callApi} >
             Submit
           </IonButton>
-          <IonButton expand="block" onClick={() => setShowModal(false)}>
-            Close
-          </IonButton>
+            <IonButton
+              expand="block"
+              
+              onClick={() => setShowModal(false)}
+            >
+              Close
+            </IonButton>
         </IonModal>
         <IonButton onClick={() => setShowModal(true)}>
           Where would you like to go
         </IonButton>
 
-        <IonItem></IonItem>
+        <IonItem>
+          <div>
+            here is the answer {callApi}
+          </div>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
 };
+
 
 export default Home;
